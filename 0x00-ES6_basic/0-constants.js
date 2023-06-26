1f52b7d2d771350f6e55aa7d28586d793cbd00e2
Zo3rb <ali.f90@outlook.com>
2023-06-26 20:36:36 +0000

    styling maybe

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -2,17 +2,17 @@
 // Using "const" and "let" Keywords to define a variables.
 
 export function taskFirst() {
-  const task = 'I prefer const when I can.';
-  return task;
+	const task = 'I prefer const when I can.';
+	return task;
 }
 
 export function getLast() {
-  return ' is okay';
+	return ' is okay';
 }
 
 export function taskNext() {
-  let combination = 'But sometimes let';
-  combination += getLast();
+	let combination = 'But sometimes let';
+	combination += getLast();
 
-  return combination;
+	return combination;
 }
